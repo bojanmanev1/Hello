370e8bd3692efe0d3f0bf96ebb0d45e3eb318fb6
bojanmanev1 <76178022+bojanmanev1@users.noreply.github.com>
2020-12-24 13:09:56 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+//1 zadaca 
+
+var carName = 'Volvo'
+
+//2 zadaca
+
+var x = 75;
+
+//4 zadaca 
+
+let x = 43;
+let y = 72;
+let z = x*y;
+alert(z)
+alert(x*y)
+
+//5 zadaca
+alert(15/9)
+alert(15%9)
+
+
+//6 zadaca
+
+x = 10;
+y = 5;
+x += y;
+console.log(x)
+
+//7 zadaca 
+x = 10;
+y = 5;
+x *= y
+console.log(x)
+
+//8 zadaca 
+let godini = 21
+if (godini >18) {
+  alert('polnoleten si')
+} else {
+  alert('ne si polnoleten')
+}
+
+//9 zadaca 
+
+let x = 10
+if (x % 2 === 0) {
+  console.log('paren')
+} else {
+  console.log('neparen')
+}
+
+
